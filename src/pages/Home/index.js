2c1e5d40517bf00693112d2cb6f7c5dc29cea882
Anastasia said <anastasiasaidfara@gmail.com>
2026-03-17 00:20:33 +0100

    rajout variable pour avoir derniere presta

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -117,12 +117,12 @@ const Page = () => {
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
         <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
-          label="boom"
-        />
+  imageSrc={last?.cover}
+  title={last?.title}
+  date={new Date(last?.date)}
+  small
+  label={last?.type}
+/>
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
