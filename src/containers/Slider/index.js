c98a5281beb7180560502148962778f44c57786f
Anastasia said <anastasiasaidfara@gmail.com>
2026-03-29 20:26:09 +0200

    correction duplication du mois de mars

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,34 +7,28 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
- const byDateDesc = data?.focus.sort((evtA, evtB) =>
-  new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
-  );
-  
+  const byDateDesc = data?.focus.sort((evtA, evtB) => (new Date(evtA.date) < new Date(evtB.date) ? 1 : -1));
+
   const nextCard = () => {
     setIndex(index < byDateDesc.length - 1 ? index + 1 : 0);
   };
-  
+
   useEffect(() => {
     const timer = setTimeout(nextCard, 5000);
     return () => clearTimeout(timer);
-  }, [index, byDateDesc]); 
-  
+  }, [index, byDateDesc]);
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
         <div key={event.title}>
-          <div
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum"/>
+          <div className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
+            <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
                 <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
+                {index === idx ? <div>{getMonth(new Date(event.date))}</div> : null}
               </div>
             </div>
           </div>
